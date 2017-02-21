6c6ba7e13b4399cee48dc3299a004e0eec328f7b
fadfood5 <Fadi@Fadis-MacBook-Pro.local>
2017-02-20 21:26:14 -0500

    added read from console and trace read

---
+++ memsim.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+//Reads trace file
+void readTrace(){
+	int c;
+	FILE *file;
+	file = fopen("bzip.trace", "r");
+	unsigned addr;
+	char rw;
+	int i = 0;
+
+	if (file) {
+	    while ((c = getc(file)) != EOF){
+					//fscanf(file, "%x %c", arr[0], arr[1]);
+					putchar(c);
+					i++;
+			}
+	    fclose(file);
+	}
+}
+
+//Reads commanf parameters from console
+int main(int argc, char **argv){
+		const char *a[2];
+    for (int i = 0; i < argc; ++i){
+        printf("argv[%d]: %s\n", i, argv[i]);
+				a[i] = argv[i];
+				printf("Added %s\n", a[i]);
+    }
+		return 0;
+}
