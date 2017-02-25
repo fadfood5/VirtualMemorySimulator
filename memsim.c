0b69603ab2ed2bd86befec737f3702ee4efdb612
fadfood5 <Fadi@Fadis-MacBook-Pro.local>
2017-02-24 21:51:34 -0500

    commented LRU

--- memsim.c
+++ memsim.c
@@ -33,25 +33,23 @@ void lru(const char *fileName, int fr, const char *type){
 			printf("Created empty struct\n");
 	}
 
-	int count = 0;
-	int count1 = 0;
-	int count2 = 0;
-	int count3 = 0;
-	int count4 = 0;
-
 	if (file) {
-			//(int i = 0; i < 10; i++){
+			//Run through file by line
 	    while ((c = getc(file)) != EOF){
-					count++;
+					//Create a temporary struct
 					struct PageTableEntry p;
 					fscanf (file, "%s %c", a, &b);
-
+					//Cut down string to page number
 					strncpy(p.pN, a, 5);
+					//Set read/write to empty
 					p.input_output = 0;
+					//Set dirty bit based on read/write being scanned
 					if(b == 'W')
 						p.dirtyBit = 1;
 					else
 						p.dirtyBit = 0;
+
+					//Debug
 					if(strcmp(type, "debug") == 0){
 							printf("%s", a);
 							printf(" %c\n", b);
@@ -59,32 +57,36 @@ void lru(const char *fileName, int fr, const char *type){
 							printf("Input: %c\n", p.input_output);
 						}
 
+					//Go through array of structs
 					for(int i = 0; i < fr; i++){
+						//If array not full and has space
 						if(strcmp(Frames[i].pN, "EMPTY") == 0){
-							count1++;
+							//Take that space
 							Frames[i] = p;
 							numReads++;
-							if(strcmp(type, "debug") == 0)
-								printf("Disk Read Performed\n");
+							//Debug
 							if(strcmp(type, "debug") == 0){
+								printf("Disk Read Performed\n");
 								printf("Free space available\n");
 								printf("Frame added: %s\n", Frames[i].pN);
 								printf("Breaking\n");
 							}
 							break;
 					}else if(strcmp(p.pN, Frames[i].pN) == 0){
-						count2++;
+							//If page number is already in array
+							//Debug
 							if(strcmp(type, "debug") == 0)
 								printf("Already exists\n");
+							//Switch input_output
 							if(Frames[i].input_output == 'R')
 								Frames[i].input_output = 'W';
 							else
 								Frames[i].input_output = 'R';
+							//Set counter to 0
 							Frames[i].counter = 0;
-						}else{
-							count4++;
-							if(i+1 == fr){
-							count3++;
+					}else if(i+1 == fr){
+								//If array full and page number not found
+								//Get highest counter
 								int max = 0;
 								int tempJ = 0;
 								for(int j = 0; j < fr; j++){
@@ -93,6 +95,7 @@ void lru(const char *fileName, int fr, const char *type){
 										tempJ = j;
 									}
 								}
+								//If highest counter's input_output is write, switch out page number
 								if(Frames[tempJ].input_output == 'W'){
 									numWrites++;
 									if(strcmp(type, "debug") == 0)
@@ -100,22 +103,18 @@ void lru(const char *fileName, int fr, const char *type){
 								Frames[tempJ] = p;
 						}
 					}
-				}
 			}
 	}
 }
-fclose(file);
-printf("Final form is: \n");
-for(int i =0; i < fr; i++){
-	Frames[i].counter++;
-	printf("%s\n", Frames[i].pN);
-	printf("%d\n", Frames[i].counter);
-}
-	printf("Count: %d\n", count);
-	printf("Count1: %d\n", count1);
-	printf("Count2: %d\n", count2);
-	printf("Count3: %d\n", count3);
-	printf("Count4: %d\n", count4);
+	//Close file
+	fclose(file);
+	//Print final array elements
+	printf("Final form is: \n");
+	for(int i =0; i < fr; i++){
+		Frames[i].counter++;
+		printf("%s\n", Frames[i].pN);
+		printf("%d\n", Frames[i].counter);
+	}
 }
 
 
