5902acc3537462bb15a5b0d4c73bc362720dd0bb
fadfood5 <Fadi@Fadis-MacBook-Pro.local>
2017-02-25 22:07:45 -0500

    changes to clk

--- memsim.c
+++ memsim.c
@@ -145,7 +145,7 @@ void clk(const char *fileName, int fr, const char *type){
 	FILE *file;
 	file = fopen(fileName, "r");
 	int i = 0;
-	char a[7];
+	char a[5];
 	char b;
 	char temp;
 
@@ -159,25 +159,26 @@ void clk(const char *fileName, int fr, const char *type){
 			printf("Created empty struct\n");
 	}
 
-	int count = 0;
-	int count1 = 0;
-	int count2 = 0;
-	int count3 = 0;
-	int count4 = 0;
-
 	if (file) {
-			//(int i = 0; i < 10; i++){
+			//Run through file by line
 	    while ((c = getc(file)) != EOF){
-					count++;
+					//Create a temporary struct
 					struct PageTableEntry p;
 					fscanf (file, "%s %c", a, &b);
-
+					//Cut down string to page number
 					strncpy(p.pN, a, 5);
+					//Set read/write to empty
+					strncpy(&p.input_output, &b, 1);
+					//Set dirty bit based on read/write being scanned
 					p.input_output = 0;
 					if(b == 'W')
 						p.dirtyBit = 1;
 					else
 						p.dirtyBit = 0;
+
+					p.counter = 0;
+					p.clk = 0;
+
 					if(strcmp(type, "debug") == 0){
 							printf("%s", a);
 							printf(" %c\n", b);
@@ -185,38 +186,44 @@ void clk(const char *fileName, int fr, const char *type){
 							printf("Input: %c\n", p.input_output);
 						}
 
+					//Go through array of structs
 					for(int i = 0; i < fr; i++){
+						//If array not full and has space
 						if(strcmp(Frames[i].pN, "EMPTY") == 0){
-							count1++;
+							//Change counter and take space
 							if(p.input_output == 'R'){
 								p.counter = 0;
-								Frames[i] = p;
-							}else{
+							else
 								p.counter = 1;
-								Frames[i] = p;
-							}
+
+							Frames[i] = p;
 							numReads++;
-							if(strcmp(type, "debug") == 0)
-								printf("Disk Read Performed\n");
+							//Debug
 							if(strcmp(type, "debug") == 0){
+								printf("Disk Read Performed\n");
 								printf("Free space available\n");
 								printf("Frame added: %s\n", Frames[i].pN);
 								printf("Breaking\n");
 							}
 							break;
 					}else if(strcmp(p.pN, Frames[i].pN) == 0){
-							count2++;
+							//If page number is already in array
+							//Debug
 							if(strcmp(type, "debug") == 0)
 								printf("Already exists\n");
-							if(Frames[i].input_output == 'W'){
-								Frames[i].input_output = 'R';
+							if(Frames[i].input_output == 'R' && p.input_output == 'W'){
+								Frames[i].input_output = 'W';
 								Frames[i].dirtyBit = 1;
 							}
+							else if(Frames[i].input_output == 'W' && p.input_output == 'R'){
+								Frames[i].input_output = 'R';
+								dirtyBit = 0;
+							}
+							//Set counter to 0
+							Frames[i].counter = 0;
 							Frames[i].clk = 1;
-						}else{
-							count4++;
+						}else
 							if(i+1 == fr){
-							count3++;
 								for(int j = 0; j < fr; j++){
 									if(Frames[j].clk == 1)
 										Frames[j].clk = 0;
@@ -234,8 +241,11 @@ void clk(const char *fileName, int fr, const char *type){
 									}
 								}
 					}
-				}
 			}
+		if(strcmp(type, "debug") == 0)
+			printf("Disk read performed\n");
+		for(int j = 0; j < fr; j++){
+			Frames[j].counter += 1;
 	}
 }
 fclose(file);
@@ -245,11 +255,10 @@ for(int i =0; i < fr; i++){
 	printf("%s\n", Frames[i].pN);
 	printf("%d\n", Frames[i].counter);
 }
-	printf("Count: %d\n", count);
-	printf("Count1: %d\n", count1);
-	printf("Count2: %d\n", count2);
-	printf("Count3: %d\n", count3);
-	printf("Count4: %d\n", count4);
+	printf("Num of traces: \n");
+	printf("Num of frames: %d\n", fr);
+	printf("Num of disk reads: %d\n", numReads);
+	printf("Num of disk writes: %d\n", numWrites);
 }
 
 void opt(const char *fileName, int fr, const char *type){
