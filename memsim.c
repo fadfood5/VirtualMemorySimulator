bd525098d9add2cb66cba29adbbcdac2db7c1bce
fadfood5 <Fadi@Fadis-MacBook-Pro.local>
2017-02-28 18:40:18 -0500

    opt changes

--- memsim.c
+++ memsim.c
@@ -31,10 +31,10 @@ void lru(const char *fileName, int fr, const char *type){
 
 	for(int i = 0; i < fr; i++){
 		struct PageTableEntry temp;
-		strncpy(temp.pN, "EMPTY", 5);
+		strncpy(temp.pN, "", 5);
 		Frames[i] = temp;
 		if(strcmp(type, "debug") == 0)
-			printf("Created empty struct\n");
+			printf("Created  struct\n");
 	}
 
 	if (file) {
@@ -48,7 +48,7 @@ void lru(const char *fileName, int fr, const char *type){
 					p.pN[5] = '\0';
 					if(strcmp(type, "debug") == 0)
 						printf("%s\n", p.pN);
-					//Set read/write to empty
+					//Set read/write to
 					strncpy(&p.input_output, &b, 1);
 					//Set dirty bit based on read/write being scanned
 					if(b == 'W')
@@ -72,7 +72,7 @@ void lru(const char *fileName, int fr, const char *type){
 					//Go through array of structs
 					for(int i = 0; i < fr; i++){
 						//If array not full and has space
-						if(strcmp(Frames[i].pN, "EMPTY") == 0){
+						if(strcmp(Frames[i].pN, "") == 0){
 							//Take that space
 							Frames[i] = p;
 							numReads++;
@@ -167,10 +167,10 @@ void clk(const char *fileName, int fr, const char *type){
 
 	for(int i = 0; i < fr; i++){
 		struct PageTableEntry temp;
-		strncpy(temp.pN, "EMPTY", 5);
+		strncpy(temp.pN, "", 5);
 		Frames[i] = temp;
 		if(strcmp(type, "debug") == 0)
-			printf("Created empty struct\n");
+			printf("Created  struct\n");
 	}
 
 	if (file) {
@@ -207,7 +207,7 @@ void clk(const char *fileName, int fr, const char *type){
 					//Go through array of structs
 					for(int i = 0; i < fr; i++){
 						//If array not full and has space
-						if(strcmp(Frames[i].pN, "EMPTY") == 0){
+						if(strcmp(Frames[i].pN, "") == 0){
 							//Take free space
 							Frames[i] = p;
 							numReads++;
@@ -316,13 +316,28 @@ void opt(const char *fileName, int fra, const char *type){
 
 	for(int i = 0; i < fr; i++){
 		struct PageTableEntry temp;
-		strncpy(temp.pN, "EMPTY", 5);
+		strncpy(temp.pN, "", 5);
 		Frames[i] = temp;
 		if(strcmp(type, "debug") == 0)
-			printf("Created empty struct\n");
+			printf("Created  struct\n");
 	}
 
 	int c1=0, c2=0, c3=0;
+
+	int counting = 0;
+	char all[1000001][8];
+	FILE *file3;
+	file3 = fopen(fileName, "r");
+	int chh;
+	if(file3){
+		while ((chh = getc(file3)) != EOF && counting <=1000001) {
+			fscanf (file3, "%s %c", all[counting], &b);
+			all[counting][5] = '\0';
+			counting++;
+		}
+	}
+	fclose(file3);
+	printf("done\n");
 
 	if (file) {
 			traces++;
@@ -358,6 +373,7 @@ void opt(const char *fileName, int fra, const char *type){
 					for(int i = 0; i < fr; i++){
 						//If array not full and has space
 						if(strcmp(Frames[i].pN, "EMPTY") == 0){
+							printf("1\n");
 							c1++;
 							//Take free space
 							Frames[i] = p;
@@ -391,86 +407,64 @@ void opt(const char *fileName, int fra, const char *type){
 								c3++;
 								int temp = count;
 
-								FILE *file2;
-								file2 = fopen(fileName, "r");
-
-								char d[8];
-								int ch;
-								countInner = 0;
-								int newlines = 0;
-								char FramesTemp[fr][8];
-								for(int i=0; i<fr; i++)
-									strcpy(FramesTemp[i], " ");
-								//printf("C3: %d\n", c3);
-								//printf("%d\n", (temp+fr-1)-(fr-1));
-								if(file2){
-									while ((ch = getc(file2)) != EOF) {
-							        if (newlines >= temp - 1 && newlines< temp+fr-1){
-													// printf("Temp: %d\n", temp-1);
-													// printf("Line: %d\n", newlines);
-													// printf("fr: %d\n", temp+fr-1);
-													// printf("countInner: %d\n", countInner);
-													fscanf (file2, "%s %c", FramesTemp[countInner], &e);
-													FramesTemp[countInner][5] = '\0';
-													//printf("%s %c\n", FramesTemp[countInner], e);
-													if(strcmp(type, "debug") == 0)
-														printf("%s\n", FramesTemp[countInner]);
-													// for(int m = 0; m < fr; m++){
-													// 	printf("FramesTemp %d: %s\n", m, FramesTemp[m]);
-													// }
-													// printf("\n");
-												countInner++;
-											}
-											if(newlines >= temp+fr+1)
-												break;
-							        newlines++;
-									}
-							}
-							fclose(file2);
-							// printf("finished\n");
-							for(int i = 0; i < fr; i++){
-								for(int j = 0; j < fr; j++){
-									if(strcmp(Frames[i].pN, FramesTemp[j]) == 0){
-										Frames[i].found = 1;
-										Frames[i].index = j;
+								char allTemp[fr][8];
+								int counting2 = 0;
+								for(int i = temp; i < temp+fr; i++){
+									if(temp+fr < 1000001){
+										//printf("Count: %d\n", temp);
+										//printf("Count2: %d\n", counting2);
+										strcpy(allTemp[counting2], all[i]);
+										allTemp[counting2][5] = '\0';
+										if(strcmp(type, "debug") == 0)
+											printf("%s\n", allTemp[counting2]);
+										counting2++;
 									}
 								}
-							}
+								if(strcmp(type, "debug") == 0)
+									printf("\n");
 
-							int max = 0;
-							if(Frames[0].found == 1)
-								max = Frames[0].index;
-
-							int test = 0;
-							int tempL = 0;
-							for(int l = 0; l<fr; l++){
-								if(Frames[l].found == 0){
-								test = 1;
-								tempL = l;
-								// printf("Test is 1\n");
-							}else{
-								if(max < Frames[l].index)
-									max = Frames[l].index;
-							}
-						}
-							if(test == 1){
-								Frames[tempL] = p;
-								numReads++;
-								numWrites++;
-							}else{
-								for(int m = 0; m<fr; m++){
-									if(max == Frames[m].index){
-										numReads++;
-										numWrites++;
+								for(int i = 0; i < fr; i++){
+									for(int j = 0; j < fr; j++){
+										if(strcmp(Frames[i].pN, allTemp[j]) == 0){
+											Frames[i].found = 1;
+											Frames[i].index = j;
+										}
 									}
 								}
-							}
 
+								int max = 0;
+								if(Frames[0].found == 1)
+									max = Frames[0].index;
+
+								int test = 0;
+								int tempL = 0;
+								for(int l = 0; l<fr; l++){
+									if(Frames[l].found == 0){
+									test = 1;
+									tempL = l;
+								}else{
+									if(max < Frames[l].index)
+										max = Frames[l].index;
+								}
+							}
+								if(test == 1){
+									Frames[tempL] = p;
+									numReads++;
+									numWrites++;
+								}else{
+									for(int m = 0; m<fr; m++){
+										if(max == Frames[m].index){
+											numReads++;
+											numWrites++;
+										}
+									}
+								}
+
+								}
 							}
 						}
-					}
-					count++;
-					 //printf("Count: %d\n", count);
+						count++;
+					 	//printf("Count: %d\n", count);
 		}
 	}
 
@@ -483,9 +477,6 @@ void opt(const char *fileName, int fra, const char *type){
 			printf("%d\n", Frames[i].counter);
 		}
 	//}
-	printf("Count1: %d\n", c1);
-	printf("countInner: %d\n", c2);
-	printf("Count3: %d\n", c3);
 	printf("Num of traces: %d\n", traces);
 	printf("Num of frames: %d\n", fr);
 	printf("Num of disk reads: %d\n", numReads);
